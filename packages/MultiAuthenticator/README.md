7e4257fc827864640179a588e895b41f9001d877
Max Kuhmichel <kuhmichel.max@fh-swf.de>
2023-03-28 15:34:48 +0200

    prep for cleaning up our Multiauth stuff

---
+++ packages/MultiAuthenticator/README.md
@@ -0,0 +1,13 @@
+## Install
+We have not published this package to pypi, but you can install it with pip directly from github:
+```
+pip install git+https://github.com/fhswf/Jupyterhub-K8s.git@main#subdirectory=packages/MultiAuthenticator
+```
+Using pip and requirements.txt, simple put the above link into the file:
+```
+package-one==1.9.4
+git+https://github.com/fhswf/Jupyterhub-K8s.git@main#subdirectory=packages/MultiAuthenticator
+package-three==2.0.0
+numpy
+...
+```
--- packages/MultiAuthenticator/setup.py
+++ packages/MultiAuthenticator/setup.py
@@ -1,6 +1,32 @@
-from setuptools import setup
+from setuptools import find_packages, setup
 
-setup(
+setup_args = dict(
     name='MultiAuthenticator',
+    packages=find_packages(),
     version='0.1.0',
+    description="JupyterHub MultiAuthenticator",
+    long_description=open("README.md").read(),
+    long_description_content_type="text/markdown",
+    author="Max Kuhmichel, FH SWF",
+    author_email="kuhmichel.max@fh-swf.de",
+    url="https://github.com/fhswf",
+    license="MIT",
+    python_requires=">=3.8",
+    include_package_data=True,
+    entry_points={
+    'jupyterhub.authenticators': [
+        'MultiAuthenticator = MultiAuthenticator.MultiAuthenticator:MultiAuthenticator',
+    ],
+  },
 )
+
+setup_args['install_requires'] = install_requires = []
+with open('requirements.txt') as f:
+    for line in f.readlines():
+        req = line.strip()
+        if not req or req.startswith(('-e', '#')):
+            continue
+        install_requires.append(req)
+
+if __name__ == '__main__':
+    setup(**setup_args)
