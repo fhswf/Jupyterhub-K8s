0901e80ee5f9c35ee63f3916276dac7cab04edd9
Max Kuhmichel <kuhmichel.max@fh-swf.de>
2023-04-14 14:50:36 +0200

    Bare bones multiauth

---
+++ packages/MultiAuthenticator/MultiAuthenticator/MultiAuthenticator.py
@@ -0,0 +1,67 @@
+import traitlets
+
+from jupyterhub.auth import Authenticator
+#from jupyterhub.handlers.login import LoginHandler, LogoutHandler
+from jupyterhub.utils import url_path_join
+#class MultiLoginHandler(LoginHandler):
+
+#class MultiLogoutHandler(LogoutHandler):
+    
+
+class MultiAuthenticator(Authenticator):
+    authenticators = traitlets.List(help="The subauthenticators to use", config=True)
+
+    def __init__(self, **kwargs):
+        super().__init__(**kwargs)  
+        self._authenticators = []
+        for authenticator_class, url_scope, display, configs in self.authenticators:
+            c = self.trait_values()
+            c.update(configs)
+            self._authenticators.append({"instance": authenticator_class(**c), "url_scope": url_scope, "display": display})
+    
+    def get_handlers(self, app):
+        routes = []
+        for _authenticator in self._authenticators:
+            for path, handler in _authenticator["instance"].get_handlers(app):
+
+                class SubHandler(handler):
+                    authenticator = _authenticator["instance"]
+
+                routes.append((f'{_authenticator["url_scope"]}{path}', SubHandler))
+        return routes
+    
+    def get_custom_html(self, base_url):
+        html = []
+        for authenticator in self._authenticators:
+            if authenticator.display == True and "login_service" in authenticator["instance"].__dict__:
+                login_service = authenticator["instance"].login_service
+                url = url_path_join(base_url, authenticator["url_scope"], "oauth_login")
+                html.append(
+                    f"""
+                    <div class="service-login">
+                    <a role="button" class='btn btn-jupyter btn-lg' href='{url}'>
+                        Sign in with {login_service}
+                    </a>
+                    </div>
+                    """
+            )
+        return "\n".join(html)
+
+    
+    async def authenticate(self, handler, data):
+        """Using the url of the request to decide which authenticator
+        is responsible for this task.
+        """
+        return self._get_responsible_authenticator(handler).authenticate(handler, data)
+
+    def get_callback_url(self, handler):
+        return self._get_responsible_authenticator(handler).get_callback_url()
+
+    def _get_responsible_authenticator(self, handler):
+        responsible_authenticator = None
+        for authenticator in self._authenticators:
+            if handler.request.path.find(authenticator['url_scope']) != -1:
+                print("redirect to ", authenticator["instance"].__name__) 
+                responsible_authenticator = authenticator
+                break
+        return responsible_authenticator['instance']
