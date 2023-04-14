b53554fcf1d3857562f92eb61971584e71ac5d18
Max Kuhmichel <kuhmichel.max@fh-swf.de>
2023-04-14 15:43:52 +0200

    ci prep

--- images/jupyterhub/_static_extra_config.py
+++ images/jupyterhub/_static_extra_config.py
@@ -12,3 +12,29 @@ print("loading extra conf: " + __file__)
 # Enable auth state to pass the authentication dictionary
 # auth_state to the spawner
 c.Authenticator.enable_auth_state = True
+
+
+import MultiAuthenticator
+from ltiauthenticator import LTIAuthenticator
+from oauthenticator.generic import GenericOAuthenticator
+
+# specific for multiauth:
+# select login options
+c.MultiAuthenticator.authenticators = [
+    (LTIAuthenticator, "", False, {
+        'LTI_CLIENT_KEY': "secretclientidforauth",
+        'LTI_SHARED_SECRET': "somesharedsecret",
+        'LTI13_PRIVATE_KEY': "/home/user/.ssh/private.pem",
+    }),
+    (GenericOAuthenticator, "keycloak", True, {
+        'client_secret': 'supersecretclientkeyforencryption',
+        'authorize_url': 'https://www.ki.fh-swf.de/keycloak/realms/kicluster/protocol/openid-connect/auth',
+        'token_url': 'https://www.ki.fh-swf.de/keycloak/realms/kicluster/protocol/openid-connect/token',
+        'userdata_url': 'https://www.ki.fh-swf.de/keycloak/realms/kicluster/protocol/openid-connect/userinfo',
+        'login_service': 'keycloak',
+        'username_key': 'preferred_username',
+        'scope': ['openid'],
+        'userdata_params': {'state': 'state'},           
+    })
+]
+c.JupyterHub.authenticator_class = MultiAuthenticator
