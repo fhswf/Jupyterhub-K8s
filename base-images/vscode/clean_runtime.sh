59f2ca46b42251f8aac32012b566b2826c2426fb
Max Kuhmichel <kuhmichel.max@fh-swf.de>
2023-07-13 14:14:18 +0200

    add ssh deamon launch

--- base-images/vscode/clean_runtime.sh
+++ base-images/vscode/clean_runtime.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 # cd cd /home/$NB_USER/.local/share/jupyter/runtime
-cd cd $HOME/.local/share/jupyter/runtime
+cd $HOME/.local/share/jupyter/runtime
 
 if [ $? -eq 0 ]; then
     rm kernel-*.json nbserver-*.html nbserver-*.json;
 else
-    cd cd /home/jovyan/.local/share/jupyter/runtime;
+    cd /home/jovyan/.local/share/jupyter/runtime;
     if [ $? -eq 0 ]; then
         rm kernel-*.json nbserver-*.html nbserver-*.json;
     fi
