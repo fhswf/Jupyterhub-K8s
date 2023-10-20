467e4c58bf36d9d10fe78ecece0397862c8e6d6f
Max Kuhmichel <kuhmichel.max@fh-swf.de>
2023-10-20 13:11:37 +0200

    make sure vars are not already defined

--- base-images/vscode/configure_cuda.sh
+++ base-images/vscode/configure_cuda.sh
@@ -2,24 +2,24 @@
 #########
 # checks gpu availabilty and sets env CUDA_VISBILE_DEVICE to use most sutiable device in case more are avaiable 
 ##########
-cmd=nvidia-smi
-if [[ $(type -P "$cmd") ]] ; then
-    mode=(`nvidia-smi --format=csv --query-gpu=mig.mode.current | tail --lines=+2`)
+kicluster_configure_cmd=nvidia-smi
+if [[ $(type -P "$kicluster_configure_cmd") ]] ; then
+    kicluster_configure_mode=(`nvidia-smi --format=csv --query-gpu=mig.mode.current | tail --lines=+2`)
     if  [ ${#mode[@]} == 0 ] ; then
        echo "WARNING: no cuda devices found";
     else
         ## mig modes for cuda devices can be: [Disabled|Enabled]
-        migs=(`nvidia-smi --format=csv --query-gpu=mig.mode.current,uuid | tail --lines=+2 | grep Enabled | awk '{print $2}'`);
-        gpus=(`nvidia-smi --format=csv --query-gpu=mig.mode.current,uuid | tail --lines=+2 | grep Disabled | awk '{print $2}'`);
-        uuids=();
-        for mig in "${migs[@]}" 
+        kicluster_configure_migs=(`nvidia-smi --format=csv --query-gpu=mig.mode.current,uuid | tail --lines=+2 | grep Enabled | awk '{print $2}'`);
+        kicluster_configure_gpus=(`nvidia-smi --format=csv --query-gpu=mig.mode.current,uuid | tail --lines=+2 | grep Disabled | awk '{print $2}'`);
+        kicluster_configure_uuids=();
+        for kicluster_configure_mig in "${kicluster_configure_migs[@]}" 
         do
-          uuid=(`nvidia-smi -L | cat - <(echo "EOF")  | grep -A10  "${mig}" | tail --lines=+2 |  grep  -B10 -E 'GPU|EOF' --max-count=1 | head --lines=-1 | awk '{print substr($6, 1, length($6)-1)}'`);
-          uuids+=("${uuid[@]}")
+          kicluster_configure_uuid=(`nvidia-smi -L | cat - <(echo "EOF")  | grep -A10  "${kicluster_configure_mig}" | tail --lines=+2 |  grep  -B10 -E 'GPU|EOF' --max-count=1 | head --lines=-1 | awk '{print substr($6, 1, length($6)-1)}'`);
+          kicluster_configure_uuids+=("${kicluster_configure_uuid[@]}")
         done;
-        uuids+=("${gpus[@]}")
+        kicluster_configure_uuids+=("${kicluster_configure_gpus[@]}")
         # pip install nvidia-ml-py
-        target_device=`python -c "  
+        kicluster_configure_target_device=`python -c "  
 import sys
 ids = sys.argv[1:]
 import pynvml as nv
@@ -39,10 +39,10 @@ s_lst = dict(sorted(lst.items(), key=operator.itemgetter(1)))
 for k,v in s_lst.items():
     print(str(k))
     break
-" ${uuids[@]} `
+" ${kicluster_configure_uuids[@]} `
     # sorted list
     fi
-    echo "Setting CUDA Device to $target_device"
-    export CUDA_VISIBLE_DEVICES=$target_device
+    echo "Setting CUDA Device to $kicluster_configure_target_device"
+    export CUDA_VISIBLE_DEVICES=$kicluster_configure_target_device
 fi;
 
