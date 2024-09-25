78f023d525b818a95577822b03f44c4dca28665a
Max Kuhmichel <kuhmichel.max@fh-swf.de>
2024-09-25 10:45:28 +0200

    Dont build at all i guess

--- images/arm32/link_files.sh
+++ images/arm32/link_files.sh
@@ -3,7 +3,7 @@ set -e
 
 if [[ ! -f "$HOME/PICO" ]] ; then
     echo "cping /opt/PICO";
-    cp -R  /opt/PICO $HOME/PICO
+    cp -R  /opt/PICO/* $HOME/PICO
 else
     echo "/opt/PICO found in $HOME";
 fi
